32a25400fef9ea7c851011ad852779e142a14c24
derekcatigan <derekcatigan03@gmail.com>
2025-09-18 10:59:37 +0800

    Contact latest changes

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -72,7 +72,7 @@ import CargoShipOverlay from '@/assets/images/stockImages/CargoShipOverlay.png';
             <ContactCard branchName="Seattle"
                 mapSrc="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2614.520757834986!2d-122.08512762390362!3d47.36330500474702!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54905fea15bd74cf%3A0xb26a2aa213c44bf1!2s19443%20SE%20266th%20St%2C%20Covington%2C%20WA%2098042%2C%20USA!5e1!3m2!1sen!2sph!4v1758115343472!5m2!1sen!2sph"
                 :contacts="[
-                    { name: 'Seattle Manila Oriental Store & Cafe 1257 A St. NE Auburn, WA 98002', title: '( Drop-off Center )', phone: '(206)-492-9123 Ext. 531-9008', email: 'pinoyprideworldwide@gmail.com' }
+                    { name: 'Seattle Manila Oriental Store & Cafe 1257 A St. NE Auburn, WA 98002', title: 'Authorized Partner', phone: '(206)-492-9123 Ext. 531-9008', email: 'pinoyprideworldwide@gmail.com' }
                 ]" />
 
             <!-- New York/New Jersey Branch -->
