cf3b24c0ed91584440bafb6ad64656cf7a59c4fb
derekcatigan <derekcatigan03@gmail.com>
2025-09-19 23:37:56 +0800

    News Changes

--- src/data/newsData.js
+++ src/data/newsData.js
@@ -1,5 +1,4 @@
-import HomeImage01 from '@/assets/images/HomeImage01.png';
-
+import { newsImages } from '@/data/newsImageData.js';
 export const newsList = [
     {
         id: 1,
@@ -7,17 +6,7 @@ export const newsList = [
         date: "Sep 19, 2025",
         description: "",
         pinned: true,
-        media: [
-            { type: "image", src: '/src/assets/images/newsImages/Sep19,2025/Image01Sep192025.jpg' },
-            { type: "image", src: '/src/assets/images/newsImages/Sep19,2025/Image02Sep192025.jpg' },
-            { type: "image", src: '/src/assets/images/newsImages/Sep19,2025/Image03Sep192025.jpg' },
-            { type: "image", src: '/src/assets/images/newsImages/Sep19,2025/Image04Sep192025.jpg' },
-            { type: "image", src: '/src/assets/images/newsImages/Sep19,2025/Image05Sep192025.jpg' },
-            { type: "image", src: '/src/assets/images/newsImages/Sep19,2025/Image06Sep192025.jpg' },
-            { type: "image", src: '/src/assets/images/newsImages/Sep19,2025/Image07Sep192025.jpg' },
-            { type: "image", src: '/src/assets/images/newsImages/Sep19,2025/Image08Sep192025.jpg' },
-            // { type: "video", src: "https://www.youtube.com/embed/dQw4w9WgXcQ" }
-        ],
+        media: newsImages["2025-09-19"],
     },
 ];
 
---
+++ src/data/newsImageData.js
@@ -0,0 +1,23 @@
+// News Images
+// Sep 19, 2025
+import Image01Sep192025 from '@/assets/images/newsImages/Sep19,2025/Image01Sep192025.jpg';
+import Image02Sep192025 from '@/assets/images/newsImages/Sep19,2025/Image02Sep192025.jpg';
+import Image03Sep192025 from '@/assets/images/newsImages/Sep19,2025/Image03Sep192025.jpg';
+import Image04Sep192025 from '@/assets/images/newsImages/Sep19,2025/Image04Sep192025.jpg';
+import Image05Sep192025 from '@/assets/images/newsImages/Sep19,2025/Image05Sep192025.jpg';
+import Image06Sep192025 from '@/assets/images/newsImages/Sep19,2025/Image06Sep192025.jpg';
+import Image07Sep192025 from '@/assets/images/newsImages/Sep19,2025/Image07Sep192025.jpg';
+import Image08Sep192025 from '@/assets/images/newsImages/Sep19,2025/Image08Sep192025.jpg';
+
+export const newsImages = {
+    "2025-09-19": [
+        { type: "image", src: Image01Sep192025 },
+        { type: "image", src: Image02Sep192025 },
+        { type: "image", src: Image03Sep192025 },
+        { type: "image", src: Image04Sep192025 },
+        { type: "image", src: Image05Sep192025 },
+        { type: "image", src: Image06Sep192025 },
+        { type: "image", src: Image07Sep192025 },
+        { type: "image", src: Image08Sep192025 },
+    ],
+};
