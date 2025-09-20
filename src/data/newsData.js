b1e6f34f913f5c05571bbafe1e2059aa1e887337
derekcatigan <derekcatigan03@gmail.com>
2025-09-20 13:15:12 +0800

    commit

--- src/data/newsData.js
+++ src/data/newsData.js
@@ -3,7 +3,7 @@ export const newsList = [
     {
         id: 1,
         title: "Pinoy Pride Worldwide visit at Penanshin office and warehouse in Manila",
-        date: "Sep 03, 2025",
+        date: "Sep 08, 2025",
         description: "",
         pinned: true,
         media: newsMedia["2025-09-19"],
